6b2adb8632a33c9b44ffb2afb1841cf281c118c0
Érico Nogueira <erico.rolim@lnls.br>
2024-06-14 16:45:45 -0300

    iocs: configure driver in st.cmd.
    Maximum camera dimension sizes are based on the constants defined for
    maximum number of columns (4096) and rows (3000) in the vendor library.
    
    Co-authored-by: Henrique F. Simoes <henrique.simoes@lnls.br>

--- iocs/tlbc2IOC/iocBoot/ioctlbc2/st.cmd
+++ iocs/tlbc2IOC/iocBoot/ioctlbc2/st.cmd
@@ -1,18 +1,21 @@
 #!../../bin/linux-x86_64/tlbc2
 
-#- You may have to change tlbc2 to something else
-#- everywhere it appears in this file
-
-#< envPaths
+< envPaths
 
 ## Register all support components
 dbLoadDatabase "../../dbd/tlbc2.dbd"
-tlbc2_registerRecordDeviceDriver(pdbbase) 
+tlbc2_registerRecordDeviceDriver(pdbbase)
 
-## Load record instances
-#dbLoadRecords("../../db/tlbc2.db","user=root")
+epicsEnvSet("PREFIX", "CED:A:TLBC2:")
+epicsEnvSet("PORT", "TLBC2")
+
+# The search path for database files
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+
+TLBC2Config("$(PORT)", 4096, 3000, 0)
+dbLoadRecords("ADBase.template", "P=$(PREFIX), R=cam1:, PORT=$(PORT), ADDR=0, TIMEOUT=1")
+
+NDStdArraysConfigure("Image1", 20, 0, "$(PORT)", 0, 0, 0, 0, 0, 5)
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX), R=image1:, PORT=Image1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT), TYPE=Int16, FTVL=USHORT, NELEMENTS=12288000")
 
 iocInit()
-
-## Start any sequence programs
-#seq snctlbc2,"user=root"
