c44a6a41eb4cac4f0f794ced7f573d3ebc8963b1
Henrique F. Simoes <henrique.simoes@lnls.br>
2024-06-20 16:09:45 -0300

    iocs: add statistics, ROI and HDF5 plugins.
    Statistics plugin may substitute taking the X and Y profiles from the
    TLBC1_Calculations' profileValuesX and profileValuesY, respectively.
    This simplifies the driver code at the cost of computing this twice.

--- iocs/tlbc2IOC/iocBoot/ioctlbc2/plugins.cmd
+++ iocs/tlbc2IOC/iocBoot/ioctlbc2/plugins.cmd
@@ -8,6 +8,14 @@
 # $(PORT)
 # The port name for the detector.
 #
+# $(MAX_IMAGE_WIDTH)
+# The maximum image width.
+# Used to set the maximum size for row profiles in the NDPluginStats plugin.
+#
+# $(MAX_IMAGE_HEIGHT)
+# The maximum image height.
+# Used to set the maximum size for column profiles in the NDPluginStats plugin.
+#
 # $(MAX_IMAGE_PIXELS)
 # The maximum number of pixels to be sent through channel access through
 # NDPluginStdArrays.
@@ -29,6 +37,14 @@
 # The queue size for all plugins.
 # Defaults to 20
 #
+# $(QSIZE_HDF5)
+# Queue size for HDF5 plugin.
+# Defaults to 50
+#
+# $(NCHANS)
+# The maximum number of time series points in the NDPluginStats plugin.
+# Defaults to 2048.
+#
 # $(MAX_THREADS)
 # The maximum number of threads for plugins which can run in multiple threads.
 # Defaults to 4.
@@ -36,3 +52,17 @@
 # Create Channel Access conversion plugin
 NDStdArraysConfigure("Image1", $(QSIZE=20), 0, $(PORT), 0, 0, 0, 0, $(MAX_THREADS=4))
 dbLoadRecords("NDStdArrays.template", "P=$(PREFIX), R=image1:, PORT=Image1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT), TYPE=$(IMAGE_ASYN_TYPE=Int16), FTVL=$(IMAGE_WAVEFORM_TYPE=USHORT), NELEMENTS=$(MAX_IMAGE_PIXELS)")
+
+# Create 1 ROI plugin
+NDROIConfigure("ROI1", $(QSIZE=20), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
+dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROI1:, PORT=ROI1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
+
+# Create 1 statistics plugin with time series
+NDStatsConfigure("STATS1", $(QSIZE=20), 0, "ROI1", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
+dbLoadRecords("NDStats.template", "P=$(PREFIX), R=Stats1:, PORT=STATS1, ADDR=0, TIMEOUT=1, HIST_SIZE=256, XSIZE=$(MAX_IMAGE_WIDTH), YSIZE=$(MAX_IMAGE_HEIGHT), NCHANS=$(NCHANS=2048), NDARRAY_PORT=ROI1")
+NDTimeSeriesConfigure("STATS1_TS", $(QSIZE=20), 0, "STATS1", 1, 23, 0, 0, 0, 0)
+dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template", "P=$(PREFIX), R=Stats1:TS:, PORT=STATS1_TS, ADDR=0, TIMEOUT=1, NDARRAY_PORT=STATS1, NDARRAY_ADDR=1, NCHANS=$(NCHANS=2048), ENABLED=1")
+
+# Configure HDF5 file format plugin
+NDFileHDF5Configure("FileHDF1", $(QSIZE_HDF5=50), 0, "$(PORT)", 0)
+dbLoadRecords("NDFileHDF5.template", "P=$(PREFIX), R=HDF1:, PORT=FileHDF1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
