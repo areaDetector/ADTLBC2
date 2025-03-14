areaDetector/ADTLBC2

commit 207435c0a65204230556a3373ddd2e3d07d99498

TLBC2App: implement temperature reading.
Temperature monitoring is important, since the device heats quite fast
and its recommended operation temperature is low. Thus, having this
exported to the control system eases making sure this is satisfied.

This is done through readFloat64, so that we can reuse the EPICS base
functionality for creating and managing scan threads. An alternative
would be to reimplement this in the driver as a poller and keeping I/O
Intr for the record [1]. Since we can override the scan value at IOC
initialization, use that to avoid replication.

Both TemperatureActual and Temperature_RBV are updated, to keep their
values valid. Note that, for the sake of simplicity, the temperature is
read twice. A refactoring should be considered to read this once
instead. Calling parameter callbacks is required to have the
Temperature_RBV updated, as it is an I/O Intr record.

[1]: https://epics.anl.gov/tech-talk/2018/msg02164.php

TLBC2App/src/TLBC2.cpp
@@ -258,6 +258,23 @@ class epicsShareClass ADTLBC2: ADDriver, epicsThreadRunable {
         }
     }
 
+    asynStatus readFloat64(asynUser *pasynUser, epicsFloat64 *value)
+    {
+        const int function = pasynUser->reason;
+
+        if (function == ADTemperatureActual) {
+            auto item = params.find(function);
+
+            auto param = std::get<Parameter<ViReal64>>(item->second);
+
+            readbackParam<ViReal64>(function, param);
+            readbackParam<ViReal64>(ADTemperature, param);
+            callParamCallbacks();
+        }
+
+        return ADDriver::readFloat64(pasynUser, value);
+    }
+
     void run() override
     {
         while (1) {
@@ -538,6 +555,7 @@ public:
         params({
             {ADAcquireTime, Parameter<ViReal64>("exposure_time", TLBC2_get_exposure_time, TLBC2_set_exposure_time, TLBC2_get_exposure_time_range)},
             {ADGain, Parameter<ViReal64>("gain", TLBC2_get_gain, TLBC2_set_gain, TLBC2_get_gain_range)},
+            {ADTemperatureActual, Parameter<ViReal64>("temperature", TLBC2_get_temperature, {})}
         })
     {
         ViUInt32 device_count = 0;

TLBC2App/Db/ADTLBC2.template
@@ -37,6 +37,10 @@ record(longout, "$(P)$(R)SizeY") {
     field(PINI, "NO")
 }
 
+record(ai, "$(P)$(R)TemperatureActual") {
+    field(SCAN, "1 second")
+}
+
 record(ao, "$(P)$(R)Attenuation") {
     field(DESC, "Set attenuation")
     field(DTYP, "asynFloat64")

commit f6a1981454a9cd707f8dc3688a11a7310c4abb75

TLBC2App: Remove ADTemperature
The ADDriver interface specifies 2 temperature PVs: ADTemperature and
ADTemperatureActual. Since ADTemperature is actually a pair of
read/write PVs, it should be used for controlling the device's
temperature, therefore ADTemperature is intended to be set to the
desired temperature. ADTemperatureActual is the actual temperature
inside the device, which can differ from ADTemperature.

Since the TLBC2 and TLBC1 libraries don't offer any control over the
device's temperature, there's no reason to set the ADTemperature PV, as
that could cause confusion if a user thinks they can control the
device's temperature.

Since we no longer need to do anything other than updating the readback
value when reading, we can make the logic more generic for any internal
float64 in the parameter library

Fixes: 207435c (TLBC2App: implement temperature reading., 2024-07-12)

TLBC2App/src/TLBC2.cpp
@@ -311,15 +311,12 @@ class epicsShareClass ADTLBC2: ADDriver, epicsThreadRunable {
     {
         const int function = pasynUser->reason;
 
-        if (function == ADTemperatureActual) {
-            auto item = params.find(function);
+        auto item = params.find(function);
 
+        if (item != params.end()) {
             auto param = std::get<Parameter<ViReal64>>(item->second);
 
             readbackParam<ViReal64>(function, param);
-            epicsFloat64 temperature;
-            getDoubleParam(function, &temperature);
-            setDoubleParam(ADTemperature, temperature);
             callParamCallbacks();
         }
 