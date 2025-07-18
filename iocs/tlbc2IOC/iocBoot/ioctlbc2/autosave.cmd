ff89db06139914a10c469e277f0a26f8faf190ea
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2025-07-18 16:58:23 -0300

    iocs: add missing load database save_restoreStatus.db
    According to the function documentation [1], the database must be loaded
    after the save_restoreSet_status_prefix function is called.
    
    [1] https://epics-modules.github.io/autosave/autoSaveRestore.html

--- iocs/tlbc2IOC/iocBoot/ioctlbc2/autosave.cmd
+++ iocs/tlbc2IOC/iocBoot/ioctlbc2/autosave.cmd
@@ -1,6 +1,6 @@
 # Save and restore setup
 
-save_restoreSet_status_prefix("$(AUTOSAVE_PREFIX)")
+save_restoreSet_status_prefix("$(PREFIX)")
 save_restoreSet_Debug(0)
 
 save_restoreSet_IncompleteSetsOk(1)
@@ -10,6 +10,8 @@ save_restoreSet_NumSeqFiles(3)
 save_restoreSet_SeqPeriodInSeconds(300)
 
 set_savefile_path("C:/ProgramData/ADTLBC2", "autosave")
+
+dbLoadRecords("db/save_restoreStatus.db", "P=$(PREFIX)")
 
 set_pass0_restoreFile("$(AUTOSAVE_PREFIX).sav")
 set_pass1_restoreFile("$(AUTOSAVE_PREFIX).sav")
