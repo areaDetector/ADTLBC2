areaDetector/ADTLBC2

commit 2b55e00dffb92c9cd3d7f80bcd6372198a16bc49

iocs: add missing load database save_restoreStatus.db
According to the function documentation [1], the database must be loaded
after the save_restoreSet_status_prefix function is called.

[1] https://epics-modules.github.io/autosave/autoSaveRestore.html

iocs/tlbc2IOC/iocBoot/ioctlbc2/autosave.cmd
@@ -1,6 +1,6 @@
 # Save and restore setup
 
-save_restoreSet_status_prefix("$(AUTOSAVE_PREFIX)")
+save_restoreSet_status_prefix("$(PREFIX)")
 save_restoreSet_Debug(0)
 
 save_restoreSet_IncompleteSetsOk(1)
@@ -10,6 +10,8 @@ save_restoreSet_NumSeqFiles(3)
 save_restoreSet_SeqPeriodInSeconds(300)
 
 set_savefile_path("C:/ProgramData/ADTLBC2", "autosave")
+
+dbLoadRecords("db/save_restoreStatus.db", "P=$(PREFIX)")
 
 set_pass0_restoreFile("$(AUTOSAVE_PREFIX).sav")
 set_pass1_restoreFile("$(AUTOSAVE_PREFIX).sav")

commit bd8e870544aa2be63e9a40b6e7c1914f1ac62c9d

iocs: load autosave db from its correct location
Autosave database should not be read from a relative path, but rather
the absolute path specified by AUTOSAVE.

Alternatively, we could configure tlbc2IOC to install required database
from $AUTOSAVE, and specify the path relative to $TOP. However, that's
not done for other database dependencies, and it is not the practice in
other example IOCs. So, let's stick with conventional direct dependence
on the installed artifact for now.

Fixes: 2b55e00 (iocs: add missing load database save_restoreStatus.db, 2025-07-18)

iocs/tlbc2IOC/iocBoot/ioctlbc2/autosave.cmd
@@ -11,7 +11,7 @@ save_restoreSet_SeqPeriodInSeconds(300)
 
 set_savefile_path("C:/ProgramData/ADTLBC2", "autosave")
 
-dbLoadRecords("db/save_restoreStatus.db", "P=$(PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(PREFIX)")
 
 set_pass0_restoreFile("$(AUTOSAVE_PREFIX).sav")
 set_pass1_restoreFile("$(AUTOSAVE_PREFIX).sav")